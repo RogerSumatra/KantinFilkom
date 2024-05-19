6b35681348a075d662a92b1e27628e4288f8f0de
Hadoroo <128714895+Hadoroo@users.noreply.github.com>
2024-05-19 15:07:19 +0700

    fix FE

--- resources/views/Web/Default/Homepage.php
+++ resources/views/Web/Default/Homepage.php
@@ -16,7 +16,7 @@
     <style>
         .nav-link:hover {
             background-color: black;
-            color: white !important;
+            color: white;
         }
 
         .nav-link {
@@ -26,11 +26,19 @@
             width: 100px;
         }
 
+        .navbar-nav {
+            display: flex;
+            flex-direction: row;
+        }
+
+        .nav-item {
+            margin-right: 10px;
+        }
+
         .search-container {
             display: flex;
             justify-content: center;
             margin-top: 30px;
-            /* Tambahkan margin top agar search bar tidak terlalu dekat dengan navigasi */
         }
 
         .form-control {
@@ -39,10 +47,6 @@
 
         .container {
             margin-top: 30px;
-        }
-
-        .navbar-toggler {
-            margin-bottom: 20px;
         }
 
         .card-link {
@@ -55,56 +59,26 @@
             transition: transform 0.2s;
         }
 
-        .carousel-control-prev-icon,
-        .carousel-control-next-icon {
-            position: relative;
-            /* Dibutuhkan untuk penggunaan pseudo-element */
-            background-color: transparent;
-            /* Mengatur latar belakang ikon menjadi transparan */
+        .daftar-toko {
+            margin-top: 70px;
+            margin-bottom: 20px;
         }
 
-        .carousel-control-prev-icon::before,
-        .carousel-control-next-icon::before {
-            content: '';
-            position: absolute;
-            top: 0;
-            right: 0;
-            bottom: 0;
-            left: 0;
-            background-color: rgba(0, 0, 0, 0.5);
-            /* Warna latar belakang yang akan diburamkan */
-            filter: blur(5px);
-            /* Menambahkan efek blur */
-            z-index: -1;
-            /* Menempatkan pseudo-element di belakang ikon */
-            border-radius: 50%;
-            /* Opsional: agar bentuk buram mengikuti bentuk ikon */
-        }
 
-        .carousel-control-prev-icon,
-        .carousel-control-next-icon {
-            z-index: 1;
-            /* Memastikan ikon tetap di atas pseudo-element */
-        }
-
-        .daftar-toko{
-            margin-bottom: 15px;
+        .footer {
+            height: 50px;
         }
     </style>
 </head>
 
 <body>
-    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+    <nav class="navbar bg-body-tertiary">
         <div class="container-fluid">
-            <a class="navbar-brand collapse" href="#">
-                <img src="img/Logo.png" alt="Kantin FILKOM" width="8%">
+            <a class="navbar-brand" href="Homepage.php">
+                <img src="img/Logo.png" alt="Kantin FILKOM" width="80px">
+
             </a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
-                data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
-                aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <div>
                 <ul class="navbar-nav ms-auto">
                     <li class="nav-item">
                         <a class="nav-link" href="Login.php">Login</a>
@@ -126,176 +100,83 @@
     </div>
 
     <div class="container text-center">
-        <div class="row daftar-toko"><h1>Daftar Toko</h1></div>
-        <div class="row justify-content-center d-none d-md-flex">
-            <div class="col-md-4">
-                <a href="" class="card-link">
-                    <div class="card mb-3">
-                        <img src="img/HomepageTop.png" class="card-img-top" alt="Toko Logo">
-                        <div class="card-body">
-                            <h5 class="card-title">Card title</h5>
-                        </div>
-                    </div>
-                </a>
-            </div>
-            <div class="col-md-4">
-                <a href="" class="card-link">
-                    <div class="card mb-3">
-                        <img src="img/HomepageTop.png" class="card-img-top" alt="Toko Logo">
-                        <div class="card-body">
-                            <h5 class="card-title">Card title</h5>
-                        </div>
-                    </div>
-                </a>
-            </div>
-            <div class="col-md-4">
-                <a href="" class="card-link">
-                    <div class="card mb-3">
-                        <img src="img/HomepageTop.png" class="card-img-top" alt="Toko Logo">
-                        <div class="card-body">
-                            <h5 class="card-title">Card title</h5>
-                        </div>
-                    </div>
-                </a>
-            </div>
-            <div class="col-md-4">
-                <a href="" class="card-link">
-                    <div class="card mb-3">
-                        <img src="img/HomepageTop.png" class="card-img-top" alt="Toko Logo">
-                        <div class="card-body">
-                            <h5 class="card-title">Card title</h5>
-                        </div>
-                    </div>
-                </a>
-            </div>
-            <div class="col-md-4">
-                <a href="" class="card-link">
-                    <div class="card mb-3">
-                        <img src="img/HomepageTop.png" class="card-img-top" alt="Toko Logo">
-                        <div class="card-body">
-                            <h5 class="card-title">Card title</h5>
-                        </div>
-                    </div>
-                </a>
-            </div>
-            <div class="col-md-4">
-                <a href="" class="card-link">
-                    <div class="card mb-3">
-                        <img src="img/HomepageTop.png" class="card-img-top" alt="Toko Logo">
-                        <div class="card-body">
-                            <h5 class="card-title">Card title</h5>
-                        </div>
-                    </div>
-                </a>
-            </div>
-            <div class="col-md-4">
-                <a href="" class="card-link">
-                    <div class="card mb-3">
-                        <img src="img/HomepageTop.png" class="card-img-top" alt="Toko Logo">
-                        <div class="card-body">
-                            <h5 class="card-title">Card title</h5>
-                        </div>
-                    </div>
-                </a>
-            </div>
+        <div class="row daftar-toko">
+            <h1>Daftar Toko</h1>
         </div>
-
-
-        <div id="cardCarousel" class="carousel slide d-md-none" data-bs-ride="carousel">
-            <!-- Carousel Indicators -->
-            <div class="carousel-indicators">
-                <button type="button" data-bs-target="#cardCarousel" data-bs-slide-to="0" class="active"
-                    aria-current="true" aria-label="Slide 1"></button>
-                <button type="button" data-bs-target="#cardCarousel" data-bs-slide-to="1" aria-label="Slide 2"></button>
-                <button type="button" data-bs-target="#cardCarousel" data-bs-slide-to="2" aria-label="Slide 3"></button>
-                <button type="button" data-bs-target="#cardCarousel" data-bs-slide-to="3" aria-label="Slide 4"></button>
-                <button type="button" data-bs-target="#cardCarousel" data-bs-slide-to="4" aria-label="Slide 5"></button>
-                <button type="button" data-bs-target="#cardCarousel" data-bs-slide-to="5" aria-label="Slide 6"></button>
-                <button type="button" data-bs-target="#cardCarousel" data-bs-slide-to="6" aria-label="Slide 7"></button>
+        <div class="row justify-content-center d-flex">
+            <div class="col-md-4">
+                <a href="Menu.php" class="card-link">
+                    <div class="card mb-4">
+                        <img src="img/HomepageTop.png" class="card-img-top" alt="Toko Logo">
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                        </div>
+                    </div>
+                </a>
             </div>
-            <div class="carousel-inner">
-                <div class="carousel-item active">
-                    <a href="" class="card-link">
-                        <div class="card mb-3">
-                            <img src="img/HomepageTop.png" class="card-img-top" alt="Toko Logo">
-                            <div class="card-body">
-                                <h5 class="card-title">Card title</h5>
-                            </div>
+            <div class="col-md-4">
+                <a href="Menu.php" class="card-link">
+                    <div class="card mb-4">
+                        <img src="img/HomepageTop.png" class="card-img-top" alt="Toko Logo">
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
                         </div>
-                    </a>
-                </div>
-                <div class="carousel-item">
-                    <a href="" class="card-link">
-                        <div class="card mb-3">
-                            <img src="img/HomepageTop.png" class="card-img-top" alt="Toko Logo">
-                            <div class="card-body">
-                                <h5 class="card-title">Card title</h5>
-                            </div>
-                        </div>
-                    </a>
-                </div>
-                <div class="carousel-item">
-                    <a href="" class="card-link">
-                        <div class="card mb-3">
-                            <img src="img/HomepageTop.png" class="card-img-top" alt="Toko Logo">
-                            <div class="card-body">
-                                <h5 class="card-title">Card title</h5>
-                            </div>
-                        </div>
-                    </a>
-                </div>
-                <div class="carousel-item">
-                    <a href="" class="card-link">
-                        <div class="card mb-3">
-                            <img src="img/HomepageTop.png" class="card-img-top" alt="Toko Logo">
-                            <div class="card-body">
-                                <h5 class="card-title">Card title</h5>
-                            </div>
-                        </div>
-                    </a>
-                </div>
-                <div class="carousel-item">
-                    <a href="" class="card-link">
-                        <div class="card mb-3">
-                            <img src="img/HomepageTop.png" class="card-img-top" alt="Toko Logo">
-                            <div class="card-body">
-                                <h5 class="card-title">Card title</h5>
-                            </div>
-                        </div>
-                    </a>
-                </div>
-                <div class="carousel-item">
-                    <a href="" class="card-link">
-                        <div class="card mb-3">
-                            <img src="img/HomepageTop.png" class="card-img-top" alt="Toko Logo">
-                            <div class="card-body">
-                                <h5 class="card-title">Card title</h5>
-                            </div>
-                        </div>
-                    </a>
-                </div>
-                <div class="carousel-item">
-                    <a href="" class="card-link">
-                        <div class="card mb-3">
-                            <img src="img/HomepageTop.png" class="card-img-top" alt="Toko Logo">
-                            <div class="card-body">
-                                <h5 class="card-title">Card title</h5>
-                            </div>
-                        </div>
-                    </a>
-                </div>
-                <!-- Repeat the above block for each card, ensuring only one has "active" class -->
+                    </div>
+                </a>
             </div>
-            <button class="carousel-control-prev" type="button" data-bs-target="#cardCarousel" data-bs-slide="prev">
-                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                <span class="visually-hidden">Previous</span>
-            </button>
-            <button class="carousel-control-next" type="button" data-bs-target="#cardCarousel" data-bs-slide="next">
-                <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                <span class="visually-hidden">Next</span>
-            </button>
+            <div class="col-md-4">
+                <a href="Menu.php" class="card-link">
+                    <div class="card mb-4">
+                        <img src="img/HomepageTop.png" class="card-img-top" alt="Toko Logo">
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                        </div>
+                    </div>
+                </a>
+            </div>
+            <div class="col-md-4">
+                <a href="Menu.php" class="card-link">
+                    <div class="card mb-4">
+                        <img src="img/HomepageTop.png" class="card-img-top" alt="Toko Logo">
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                        </div>
+                    </div>
+                </a>
+            </div>
+            <div class="col-md-4">
+                <a href="Menu.php" class="card-link">
+                    <div class="card mb-4">
+                        <img src="img/HomepageTop.png" class="card-img-top" alt="Toko Logo">
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                        </div>
+                    </div>
+                </a>
+            </div>
+            <div class="col-md-4">
+                <a href="Menu.php" class="card-link">
+                    <div class="card mb-4">
+                        <img src="img/HomepageTop.png" class="card-img-top" alt="Toko Logo">
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                        </div>
+                    </div>
+                </a>
+            </div>
+            <div class="col-md-4">
+                <a href="Menu.php" class="card-link">
+                    <div class="card mb-4">
+                        <img src="img/HomepageTop.png" class="card-img-top" alt="Toko Logo">
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                        </div>
+                    </div>
+                </a>
+            </div>
         </div>
     </div>
+    <div class="footer"></div>
 
 
 </body>
--- resources/views/Web/Default/Login.php
+++ resources/views/Web/Default/Login.php
@@ -11,30 +11,61 @@
         crossorigin="anonymous"></script>
 
     <style>
-        body,
-        html {
-            height: 100%;
-            margin: 0;
-            padding: 0;
+        body {
+            background-image: url("img/Background.png");
+            background-size: 100% 100%;
+            background-position: center;
+            background-repeat: no-repeat;
+            background-attachment: fixed;
         }
 
-        #background-image {
-            position: absolute;
+        body::after {
+            content: "";
+            background: url("img/Background.png");
+            background-size: cover;
+            background-position: center;
+            background-repeat: no-repeat;
+            filter: blur(2px);
+            position: fixed;
             top: 0;
             left: 0;
-            height: 100%;
             width: 100%;
-            object-fit: cover;
+            height: 100%;
             z-index: -1;
+        }
+
+
+
+        .container {
+            text-align: center;
+            border: 2px solid black;    
+            margin: 20px auto;
+            width: fit-content;
+            background-image: url("img/Background.png");
+            background-size: cover;
+            background-position: center;
+            height: 95vh;
+        }
+
+        .white-background {
+            background-color: white;
+
         }
     </style>
 </head>
 
 <body>
-    <img src="img/Background.png" alt="" id="background-image">
-    <div class="container">
-        <div></div>
-        <div></div>
+    <div class="container text-center">
+        <div class="row">
+            <div class="col white-background justify-content-center">
+                <h1>Pengen Pesen Makan Tapi Males Ngantri? Kantin FILKOM Aja</h1>
+                <input type="text"><br>
+                <input type="text"><br>
+                <button>Login</button><br>
+                <a href="">Tidak punya akun? buat sekarang</a>
+            </div>
+            <div class="col"></div>
+        </div>
     </div>
 </body>
 
