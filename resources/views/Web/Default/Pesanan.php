d7b2cfa206a41cbd200bf89b959b75e05f4e51a9
Salma Adzra Fathina <salmafthn@student.ub.ac.id>
2024-05-22 18:14:50 +0700

    Pesanan1

---
+++ resources/views/Web/Default/Pesanan.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pesanan</title>
+
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+
+    <style>
+        .header-history {
+            border-bottom: 1px solid black;
+        }
+
+        .image-pesanan {
+            margin: 20px 0px 20px 0px;
+            width: 100px;
+            height: auto;
+        }
+    </style>
+
+</head>
+
+<body>
+    <main>
+        <div class="container-lg">
+            <div class="header-history">
+                <h6>Pesanan</h6>
+            </div>
+            <div class="content">
+                <div class="row">
+                    <div class="col">
+                        <div class="card mb-4">
+                            <div class="row">
+                                <div class="menu-image col-md-6">
+                                    <img src="img/HomepageTop.png" class="card-img" alt="...">
+                                </div>
+                                <div class="col-md-6">
+                                    <div class="card-body text-center">
+                                        <h5 class="card-title">Card title</h5>
+                                        <div class="d-flex justify-content-between align-items-center">
+                                            <p class="m-0">harga</p>
+                                            <button class="btn btn-outline-success" type="submit">Tambah</button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <img src="img/HomepageTop.png" alt="" class="image-pesanan">
+                        <p>Nama Resto</p>
+                    </div>
+                    <div class="col">
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </main>
+
+</body>
+
+</html>
