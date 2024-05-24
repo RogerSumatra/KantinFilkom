099c1248c5dcaa491a02904777026c6ec4d94490
Hadoroo <128714895+Hadoroo@users.noreply.github.com>
2024-05-24 18:18:40 +0700

    qwe

--- app/Http/Controllers/WebController.php
+++ app/Http/Controllers/WebController.php
@@ -7,8 +7,15 @@ use Illuminate\Http\Request;
 
 class WebController extends Controller
 {
-    public function test(){
-        return view('menu');
+    public function test()
+    {
+        return view('web.homepage');
+    }
+
+    public function index()
+    {
+        $toko = 5; // atau logika Anda untuk menentukan jumlah toko
+        return view('homepage', compact('toko'));
     }
 
 }
