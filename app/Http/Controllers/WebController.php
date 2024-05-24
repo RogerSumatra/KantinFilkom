66e1d1b2973c49560a869dccb648994485173f19
Salma Adzra Fathina <salmafthn@student.ub.ac.id>
2024-05-24 18:27:28 +0700

    ss

--- app/Http/Controllers/WebController.php
+++ app/Http/Controllers/WebController.php
@@ -7,10 +7,6 @@ use Illuminate\Http\Request;
 
 class WebController extends Controller
 {
-<<<<<<< HEAD
-    public function test(){
-        return view('welcome');
-=======
     public function test()
     {
         return view('web.homepage');
@@ -20,7 +16,6 @@ class WebController extends Controller
     {
         $toko = 5; // atau logika Anda untuk menentukan jumlah toko
         return view('homepage', compact('toko'));
->>>>>>> 099c1248c5dcaa491a02904777026c6ec4d94490
     }
 
 }
