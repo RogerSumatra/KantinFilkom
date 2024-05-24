a6e13257565ba433c1b9e750bdd876fb7b4a2883
Salma Adzra Fathina <salmafthn@student.ub.ac.id>
2024-05-24 20:52:26 +0700

    pesanan

--- resources/views/Web/Navbar_php/navbar_loggedin.php
+++ resources/views/Web/Navbar_php/navbar_loggedin.php
@@ -1,17 +1,15 @@
 <?php
 $item = 5;
+$total_harga = "eek";
 ?>
 
 <!DOCTYPE html>
 <html>
 
 <head>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
-        crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 
     <style>
         .nav-link {
@@ -67,6 +65,10 @@ $item = 5;
 
         .card-body h5 {
             margin-top: 5px;
+        }
+
+        .total-harga {
+            margin: auto;
         }
     </style>
 </head>
@@ -142,12 +144,18 @@ $item = 5;
 
                     </div>
                     <div class="modal-footer">
-                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                        <p class="total-harga">
+                            <?php
+                            echo $total_harga;
+                            ?>
+                        </p>
+
                         <button type="button" class="btn btn-primary">Konfirmasi Pesanan</button>
                     </div>
                 </div>
             </div>
         </div>
+    </div>
 </body>
 
 </html>
