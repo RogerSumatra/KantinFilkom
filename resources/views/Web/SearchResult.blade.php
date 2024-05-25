61c1aa3989d5af5aa1049b72992c3b65cc7969b7
Daffarel Adyatma Winuradmaja <farelalden96@gmail.com>
2024-05-26 01:32:16 +0700

    add navebar to seach

--- resources/views/Web/SearchResult.blade.php
+++ resources/views/Web/SearchResult.blade.php
@@ -11,9 +11,12 @@ $nama_toko = "modol";
 
 <head>
     <title>Kantin FILKOM | Homepage</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
+        crossorigin="anonymous"></script>
     <style>
         .search-container {
             display: flex;
@@ -38,9 +41,17 @@ $nama_toko = "modol";
 </head>
 
 <body>
+
+    @auth
+        @include('web.loggedin')
+    @else
+        @include('web.default')
+    @endauth
+    
     <div>
         <form class="search-container d-flex" role="search" action="{{route('menus.search')}}" method="GET">
-            <input class="form-control me-2" type="search" placeholder="Mau makan apa?" aria-label="Search" name="query">
+            <input class="form-control me-2" type="search" placeholder="Mau makan apa?" aria-label="Search"
+                name="query">
             <button class="btn btn-outline-success" type="submit">Search</button>
         </form>
     </div>
@@ -51,47 +62,47 @@ $nama_toko = "modol";
 
     <div class="row justify-content-center d-flex px-5">
         @foreach ($menus as $item)
-        <div class="col-md-12">
-            <div class="card mb-4">
-                <div class="row">
-                    <div class="col-md-9">
-                        <div class="card-body">
-                            <h5 class="nama-toko" id="nama-toko">
+            <div class="col-md-12">
+                <div class="card mb-4">
+                    <div class="row">
+                        <div class="col-md-9">
+                            <div class="card-body">
+                                <h5 class="nama-toko" id="nama-toko">
 
-                                @foreach ($toko as $tokos)
-                                @if ($tokos->id==$item->seller_id)
-                                {{$tokos->nama_toko}}
-                                @endif
-                                @endforeach
-                            </h5>
+                                    @foreach ($toko as $tokos)
+                                        @if ($tokos->id == $item->seller_id)
+                                            {{$tokos->nama_toko}}
+                                        @endif
+                                    @endforeach
+                                </h5>
+                            </div>
+                        </div>
+                        <div class="col-md-3">
+                            <div class="card-body text-end">
+                                <button class="btn btn-outline-success" type="submit">Lihat Toko</button>
+                            </div>
                         </div>
                     </div>
-                    <div class="col-md-3">
-                        <div class="card-body text-end">
-                            <button class="btn btn-outline-success" type="submit">Lihat Toko</button>
+                    <div class="row align-items-center">
+                        <div class="col-md-4 d-flex justify-content-center align-items-center">
+                            <img src="img/HomepageTop.png" class="card-img" alt="...">
                         </div>
-                    </div>
-                </div>
-                <div class="row align-items-center">
-                    <div class="col-md-4 d-flex justify-content-center align-items-center">
-                        <img src="img/HomepageTop.png" class="card-img" alt="...">
-                    </div>
-                    <div class="col-md-8">
-                        <div class="card-body">
-                            <h5 class="card-title">
-                                {{$item->menus_name}}
-                            </h5>
-                            <div class="d-flex justify-content-between align-items-center">
-                                <p class="harga-menu">
-                                    {{$item->menus_price}}
-                                </p>
-                                <button class="btn btn-outline-success" type="submit">Tambah</button>
+                        <div class="col-md-8">
+                            <div class="card-body">
+                                <h5 class="card-title">
+                                    {{$item->menus_name}}
+                                </h5>
+                                <div class="d-flex justify-content-between align-items-center">
+                                    <p class="harga-menu">
+                                        {{$item->menus_price}}
+                                    </p>
+                                    <button class="btn btn-outline-success" type="submit">Tambah</button>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
         @endforeach
 
 
