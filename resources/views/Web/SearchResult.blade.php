264a94baa77f6b2f6fc791ccc3f90784d8f6fbd2
Salma Adzra Fathina <salmafthn@student.ub.ac.id>
2024-05-26 09:21:56 +0700

    serc

--- resources/views/Web/SearchResult.blade.php
+++ resources/views/Web/SearchResult.blade.php
@@ -3,12 +3,9 @@
 
 <head>
     <title>Kantin FILKOM | Homepage</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
-        crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
     <style>
         .search-container {
             display: flex;
@@ -34,26 +31,26 @@
 
 <body>
 
+
     @auth
-        @include('web.loggedin')
+    @include('web.loggedin')
     @else
-        @include('web.default')
+    @include('web.default')
     @endauth
+    <div class="container-lg">
+        <div>
+            <form class="search-container d-flex" role="search" action="{{route('menus.search')}}" method="GET">
+                <input class="form-control me-2" type="search" placeholder="Mau makan apa?" aria-label="Search" name="query">
+                <button class="btn btn-outline-success" type="submit">Search</button>
+            </form>
+        </div>
 
-    <div>
-        <form class="search-container d-flex" role="search" action="{{route('menus.search')}}" method="GET">
-            <input class="form-control me-2" type="search" placeholder="Mau makan apa?" aria-label="Search"
-                name="query">
-            <button class="btn btn-outline-success" type="submit">Search</button>
-        </form>
-    </div>
+        <h3 class="search-text">
+            Toko yang memiliki "{{$query}}"
+        </h3>
 
-    <h3 class="search-text">
-        Toko yang memiliki "{{$query}}"
-    </h3>
-
-    <div class="row justify-content-center d-flex px-5">
-        @foreach ($menus as $item)
+        <div class="row justify-content-center d-flex px-5">
+            @foreach ($menus as $item)
             <div class="col-md-12">
                 <div class="card mb-4">
                     <div class="row">
@@ -62,9 +59,9 @@
                                 <h5 class="nama-toko" id="nama-toko">
 
                                     @foreach ($toko as $tokos)
-                                        @if ($tokos->id == $item->seller_id)
-                                            {{$tokos->nama_toko}}
-                                        @endif
+                                    @if ($tokos->id == $item->seller_id)
+                                    {{$tokos->nama_toko}}
+                                    @endif
                                     @endforeach
                                 </h5>
                             </div>
@@ -95,11 +92,11 @@
                     </div>
                 </div>
             </div>
-        @endforeach
+            @endforeach
 
 
+        </div>
     </div>
-
 </body>
 
 </html>
