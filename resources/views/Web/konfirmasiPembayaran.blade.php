0fd659606fbea242699af1c9ac888b305f1d32d6
Hadoroo <128714895+Hadoroo@users.noreply.github.com>
2024-05-27 01:05:24 +0700

    be fix

--- resources/views/Web/konfirmasiPembayaran.blade.php
+++ resources/views/Web/konfirmasiPembayaran.blade.php
@@ -71,18 +71,15 @@
         <div class="modal-dialog modal-dialog-centered">
             <div class="modal-content">
                 <div class="modal-header">
-                    
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    <form action="{{route('homepage')}}">
+                        <button type="submit" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </form>
                 </div>
                 <div class="modal-body row justify-content-center">
                     <img class="check-image" src="{{asset('img/checked.png')}}" alt="">
                     <div class="modal-text">
                         Hore, pembayaran berhasil
                     </div>
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                    <button type="button" class="btn btn-primary">Save changes</button>
                 </div>
             </div>
         </div>
